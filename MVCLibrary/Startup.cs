105ea4b13cc1b6b305e7c89bbe946484087d2b5b
Mateusz Werpachowski <matyszxzx@gmail.com>
2017-10-16 17:16:05 +0200

    Add project files.

---
+++ MVCLibrary/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCLibrary.Startup))]
+namespace MVCLibrary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
