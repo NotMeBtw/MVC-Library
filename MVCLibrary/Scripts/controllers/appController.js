3feefdc6d7d8133601274b4ce96e30c52c7193d4
Mateusz Werpachowski <matyszxzx@gmail.com>
2017-12-18 22:53:21 +0100

    Added edit mode for books and categories

---
+++ MVCLibrary/Scripts/controllers/appController.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module('mvcLibrary', []);
+
+app.controller('booksController', function ($scope) {
+    $scope.editmode = 0;
+
+    $scope.setEditMode = function (id) {
+        $scope.editmode = id;
+    };
+    $scope.cancel = function () {
+        $scope.editmode = 0;
+    };
+    $scope.submit = function () {
+        console.log('Submit');
+    };
+});
+
+app.controller('categoriesController', function ($scope) {
+    $scope.editmode = '';
+
+    $scope.setEditMode = function (name) {
+        $scope.editmode = name;
+    };
+    $scope.cancel = function () {
+        $scope.editmode = '';
+    };
+    $scope.submit = function () {
+        console.log('Submit');
+    };
+});
